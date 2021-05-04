1b370f7cae4f30db9190d7f21458847064cf9e9a
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-04 19:50:57 +0100

    major refactor, new emojis

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const feelingToBeremoji = (feeling) => {
-  switch (feeling) {
-    case "celebration":
-      return "🥳";
-    case "focus":
-      return "🔨";
-    case "launch":
-      return "🚀";
-    case "love":
-      return "😍";
-  }
+const beremojiIndex = {
+  feelings: {
+    rocket: 0,
+    launch: 0,
+    takeoff: 0,
+    celebrate: 1,
+    party: 1,
+    build: 2,
+    develop: 2,
+    mine: 2,
+    focus: 2,
+    love: 3,
+    inlove: 3,
+    fire: 4,
+    epic: 4,
+    cowboy: 5,
+    feelinggood: 5,
+    happy: 5,
+    bereket: 5,
+    face: 5,
+  },
+  emojis: {
+    0: "🚀",
+    1: "🥳",
+    2: "🔨",
+    3: "😍",
+    4: "🔥",
+    5: "🤠",
+  },
 };
 
-module.exports = { convertFeeling: feelingToBeremoji };
+const convertFeeling = (feeling) =>
+  beremojiIndex.emojis[beremojiIndex.feelings[feeling]];
+
+module.exports = { convertFeeling };
